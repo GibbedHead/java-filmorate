40b0f3ee2704dee3ab6d243f24ba9888823af309
GibbedHead <chaplygin-ma@yandex.ru>
2023-04-18 21:25:46 +0300

    fix: FilmNotFoundException.java:7:53: WhitespaceAround: '{' is not preceded with whitespace.

--- src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class FilmNotFoundException extends Exception{
+public class FilmNotFoundException extends Exception {
     public FilmNotFoundException(String message) {
         super(message);
     }
