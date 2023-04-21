95febab7c7aefc3a5b58860cce8799bc9a3cc261
GibbedHead <chaplygin-ma@yandex.ru>
2023-04-21 12:59:26 +0300

    feat: SQL файлы структуры базы и тестового наполнения feat: Модели жанра и рейтинга

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Rating.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class Rating {
+    long id;
+    String name;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,28 @@
+INSERT INTO PUBLIC.GENRE (NAME) VALUES
+	 ('Комедия'),
+	 ('Драма'),
+	 ('Мультфильм'),
+	 ('Триллер'),
+	 ('Документальный'),
+	 ('Боевик');
+INSERT INTO PUBLIC.RATING (NAME) VALUES
+	 ('G'),
+	 ('PG'),
+	 ('PG-13'),
+	 ('R'),
+	 ('NC-17');
+INSERT INTO PUBLIC.FILM (NAME,DESCRIPTION,RELEASE_DATE,DURATION,RATING_ID) VALUES
+	 ('Фильм1','Описание 1','1999-09-09',110,2),
+	 ('Фильм2','Описание2','1990-05-05',120,3);
+INSERT INTO PUBLIC.USERS (EMAIL,LOGIN,NAME,BIRTHDAY) VALUES
+	 ('user1@dom.com','user1','name1','2000-02-02'),
+	 ('user2@dom.com','user2','name2','1995-08-08'),
+	 ('user3@dom.com','user3','name3','1955-09-09');
+INSERT INTO PUBLIC.FILM_GENRE (FILM_ID,GENRE_ID) VALUES
+	 (1,2),
+	 (1,4),
+	 (2,5);
+INSERT INTO PUBLIC.FILM_LIKES (FILM_ID,USER_ID) VALUES
+	 (1,1),
+	 (1,2),
+	 (2,3);
