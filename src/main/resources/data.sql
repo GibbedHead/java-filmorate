d526a09aab814095e65a775dc816187a1cee1960
GibbedHead <chaplygin-ma@yandex.ru>
2023-04-21 16:28:29 +0300

    refactor: Переделал хранение данных в таблицах связях чтобы первичный ключ был состаным и подготовил под это новые sql файлы со структурой и данными

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,28 +1,38 @@
-INSERT INTO PUBLIC.GENRE (NAME) VALUES
+INSERT INTO PUBLIC.GENRE (GENRE_NAME) VALUES
 	 ('Комедия'),
 	 ('Драма'),
 	 ('Мультфильм'),
 	 ('Триллер'),
 	 ('Документальный'),
 	 ('Боевик');
-INSERT INTO PUBLIC.RATING (NAME) VALUES
+	 
+INSERT INTO PUBLIC.RATING (RATING_NAME) VALUES
 	 ('G'),
 	 ('PG'),
 	 ('PG-13'),
 	 ('R'),
 	 ('NC-17');
+	 
 INSERT INTO PUBLIC.FILM (NAME,DESCRIPTION,RELEASE_DATE,DURATION,RATING_ID) VALUES
 	 ('Фильм1','Описание 1','1999-09-09',110,2),
 	 ('Фильм2','Описание2','1990-05-05',120,3);
+	 
 INSERT INTO PUBLIC.USERS (EMAIL,LOGIN,NAME,BIRTHDAY) VALUES
 	 ('user1@dom.com','user1','name1','2000-02-02'),
 	 ('user2@dom.com','user2','name2','1995-08-08'),
 	 ('user3@dom.com','user3','name3','1955-09-09');
+	 
 INSERT INTO PUBLIC.FILM_GENRE (FILM_ID,GENRE_ID) VALUES
 	 (1,2),
 	 (1,4),
 	 (2,5);
+	 
 INSERT INTO PUBLIC.FILM_LIKES (FILM_ID,USER_ID) VALUES
 	 (1,1),
 	 (1,2),
 	 (2,3);
+
+INSERT INTO PUBLIC.FRIENDS (REQUESTER_ID,ACCEPTER_ID,STATUS) VALUES
+	 (1,2,'requested'),
+	 (1,3,'accepted'),
+	 (2,3,'accepted');
