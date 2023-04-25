322e3a45916770ffb97e4210545036849377d2d2
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-04-25 14:00:56 +0300

    feat: сделала метод findById

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -6,24 +6,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class Review {
-    long reviewId;
-    @NotBlank
+    long id;
     String content;
-    @NotNull
     @JsonProperty(value = "isPositive")
-    Boolean isPositive;
-    @NotNull
-    Long userId;
-    @NotNull
-    Long filmId;
+    boolean isPositive;
+    long userId;
+    long filmId;
     int useful;
-
 }
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,18 @@ INSERT INTO PUBLIC.MPA (MPA_NAME) VALUES
 	 ('PG-13'),
 	 ('R'),
 	 ('NC-17');
+
+INSERT INTO public.film(name, description, release_date, duration, mpa_id) VALUES ('Титаник','Любовь на корабле','1991-09-21',240,1);
+
+INSERT INTO film_genre(film_id, genre_id) VALUES (1, 2);
+INSERT INTO film_genre(film_id, genre_id) VALUES (1, 5);
+
+INSERT INTO public.film(name, description, release_date, duration, mpa_id) VALUES ('Убить Билла','Месть как она есть','1995-01-01',180,4);
+
+INSERT INTO film_genre(film_id, genre_id) VALUES (2, 6);
+
+INSERT INTO public.users(email,login,name,birthday) VALUES ('v.v_ivanova@mail.ru','veronaIV','Veronika Matveeva','1989-01-19');
+INSERT INTO public.users(email,login,name,birthday) VALUES ('sever_pomnit@mail.ru','sevyndel','Sever Matveev','2021-11-26');
+INSERT INTO public.users(email,login,name,birthday) VALUES ('matway@mail.ru','arturM','Artur Matveev','1985-01-02');
+
+INSERT INTO PUBLIC.REVIEWS (CONTENT, IS_POSITIVE, USER_ID, FILM_ID) VALUES ('Слезовыжималка', false, 1, 1)
