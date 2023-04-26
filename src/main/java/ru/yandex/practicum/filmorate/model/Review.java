4bfc3a529673815840d1b34b0b7c049194f481b9
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-04-26 22:47:08 +0300

    fix: 10 тестов ок

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -6,16 +6,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class Review {
     long reviewId;
+    @NotBlank
     String content;
+    @NotNull
     @JsonProperty(value = "isPositive")
-    boolean isPositive;
-    long userId;
-    long filmId;
+    Boolean isPositive;
+    @NotNull
+    Long userId;
+    @NotNull
+    Long filmId;
     int useful;
 
 }
