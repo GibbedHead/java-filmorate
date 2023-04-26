e1bc834e774bef68f6f86457039ec699a354fd3f
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-04-26 21:51:18 +0300

    fix: 6 тест ок

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,6 +13,7 @@ INSERT INTO PUBLIC.MPA (MPA_NAME) VALUES
 	 ('R'),
 	 ('NC-17');
 
+	 
 INSERT INTO public.film(name, description, release_date, duration, mpa_id) VALUES ('Титаник','Любовь на корабле','1991-09-21',240,1);
 
 INSERT INTO film_genre(film_id, genre_id) VALUES (1, 2);
@@ -25,4 +26,5 @@ INSERT INTO film_genre(film_id, genre_id) VALUES (2, 6);
 INSERT INTO public.users(email,login,name,birthday) VALUES ('v.v_ivanova@mail.ru','veronaIV','Veronika Matveeva','1989-01-19');
 INSERT INTO public.users(email,login,name,birthday) VALUES ('sever_pomnit@mail.ru','sevyndel','Sever Matveev','2021-11-26');
 INSERT INTO public.users(email,login,name,birthday) VALUES ('matway@mail.ru','arturM','Artur Matveev','1985-01-02');
+
 
